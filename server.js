f1cf9908a18641e385dcf2b7b63c042b92111cde
MourichidatouBIOKOU <mourbiokou8@gmail.com>
2025-11-30 20:40:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const path = require("path");
-
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 // Le dossier contenant le build Angular
 const angularDistPath = path.join(__dirname, "dist/assigment-app/browser");
 
@@ -19,3 +20,4 @@ const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
